3bd7636b0c06ef8d84dd97ed10491ff2a06da6fe
bheutschi <bheutschi@gmail.com>
2026-05-19 15:45:45 +0200

    feat: add Pokémon display functionality in MainActivity and update layout

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.pokedex
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.pokedex.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,6 +15,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        displayPokemon(1)
+    }
 
+    private fun displayPokemon(id: Int) {
+        lifecycleScope.launch {
+            try {
+                val pokemon = RetrofitClient.api.getPokemon(id.toString())
+                binding.pokemonName.text = pokemon.name.fr
+            } catch (e: Exception) {
+                binding.pokemonName.text = "Erreur de chargement"
+                e.printStackTrace()
+            }
+        }
     }
 }
