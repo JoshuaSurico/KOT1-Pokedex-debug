9bb1312c9a7bf154d1c8c2074087a4e39b0500f6
bheutschi <bheutschi@gmail.com>
2026-05-20 08:34:51 +0200

    feat: add Pokémon sprite display in MainActivity and update layout

--- app/src/main/java/com/example/pokedex/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
 import com.example.pokedex.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
 
@@ -18,10 +19,16 @@ class MainActivity : AppCompatActivity() {
         displayPokemon(1)
     }
 
+    private var currentPokemon: Pokemon? = null
+
     private fun displayPokemon(id: Int) {
         lifecycleScope.launch {
             try {
                 val pokemon = RetrofitClient.api.getPokemon(id.toString())
+                currentPokemon = pokemon
+                Glide.with(this@MainActivity)
+                    .load(pokemon.sprites.regular)
+                    .into(binding.imgSprite)
                 binding.pokemonName.text = pokemon.name.fr
             } catch (e: Exception) {
                 binding.pokemonName.text = "Erreur de chargement"
